25b90778865b48163c45f1049eeaa695c8a43be7
astoycos <astoycos@redhat.com>
2022-11-08 17:36:46 -0500

    tmp working on bpf_csum_diff

--- tc-udp-redirect/tc-udp-redirect-ebpf/src/main.rs
+++ tc-udp-redirect/tc-udp-redirect-ebpf/src/main.rs
@@ -8,9 +8,11 @@ use aya_bpf::{
     macros::{classifier, map},
     maps::{HashMap},
     programs::TcContext,
+    helpers::{bpf_csum_diff, bpf_l3_csum_replace}
 };
 use aya_log_ebpf::info;
 use memoffset::offset_of;
+use mem::size_of;
 
 use tc_udp_redirect_common::{Backend, VipKey};
 
@@ -38,6 +40,19 @@ pub struct Buf {
     pub buf: [u8; BUF_CAPACITY],
 }
 
+#[inline(always)] // (2)
+unsafe fn ptr_at<T>(ctx: &TcContext, offset: usize) -> Result<*mut T, i64> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(TC_ACT_OK.into());
+    }
+
+    Ok((start + offset) as *mut T)
+}
+
 #[map(name = "BACKENDS")] 
 static mut BACKENDS: HashMap<VipKey, Backend> =
     HashMap::<VipKey, Backend>::with_max_entries(128, 0);
@@ -57,7 +72,17 @@ fn get_backend(key: VipKey) -> Option<&'static Backend> {
     unsafe { BACKENDS.get(&key) }
 }
 
-fn try_tc_udp_redirect(ctx: TcContext) -> Result<i32, i64> {    
+fn csum_fold_helper(mut csum: u64) -> u16 {
+    for i in 0..4
+    {
+        if (csum >> 16) > 0 {
+            csum = (csum & 0xffff) + (csum >> 16);
+        }
+    }
+    return !(csum as u16);
+}
+
+fn try_tc_udp_redirect(mut ctx: TcContext) -> Result<i32, i64> {    
     let h_proto = u16::from_be(
         ctx.load(offset_of!(ethhdr, h_proto))
             .map_err(|_| TC_ACT_PIPE)?,
@@ -77,35 +102,62 @@ fn try_tc_udp_redirect(ctx: TcContext) -> Result<i32, i64> {
 
     //let dst_ip = u32::from_be(ctx.load(ETH_HDR_LEN + offset_of!(iphdr, daddr))?);
 
-    let ip_hdr: iphdr = ctx.load(ETH_HDR_LEN)?;
+    let ip_hdr: *mut iphdr = unsafe { ptr_at(&ctx, ETH_HDR_LEN) }?;
 
     let udp_header_offset = ETH_HDR_LEN + IP_HDR_LEN;
 
     //let dst_port = u16::from_be(ctx.load(offset + offset_of!(udphdr, dest))?);
-    let udp_hdr: udphdr = ctx.load(udp_header_offset)?;
+    let udp_hdr: *mut udphdr = unsafe { ptr_at(&ctx, udp_header_offset)? };
     
     let key = VipKey{
-        vip: u32::from_be(ip_hdr.daddr), 
-        port: (u16::from_be(udp_hdr.dest)) as u32,
+        vip: u32::from_be(unsafe { (*ip_hdr).daddr }), 
+        port: (u16::from_be(unsafe { (*udp_hdr).dest })) as u32,
     };
 
-    let backend = get_backend(key);
+    let backend = get_backend(key).ok_or(TC_ACT_OK)?;
 
-    if backend.is_none() { 
-        return Ok(TC_ACT_OK)
+    info!(&ctx, "Received a packet destined for svc ip: {:X} at port: {}",
+        u32::from_be(unsafe { (*ip_hdr).daddr }),
+        u16::from_be(unsafe { (*udp_hdr).dest })
+    );
+
+    // Load Src/Dest IPs, update IP cksum, Zero Out UDP Cksum
+    // unsafe { (*ip_hdr).check = 0; }
+    //let raw_backend_daddr = backend.daddr.to_be() as *mut _;
+    //let l3_sum = unsafe { bpf_csum_diff( (*ip_hdr).daddr as *mut u32,4, backend.daddr.to_be() as *mut u32, 4, 0) };
+    //unsafe { (*ip_hdr).daddr = backend.daddr.to_be(); }
+ 
+    //ctx.l3_csum_replace(ETH_HDR_LEN + offset_of!(iphdr, check), 0, 100 as u64, 0);
+    
+
+    //ctx.l3_csum_replace(ETH_HDR_LEN + offset_of!(iphdr, check), unsafe { (*ip_hdr).daddr } as u64, backend.daddr.to_be() as u64, 4);
+    unsafe { (*ip_hdr).daddr = backend.daddr.to_be(); }
+    //unsafe { (*ip_hdr).check = 0; }
+
+    if (ctx.data() + ETH_HDR_LEN + size_of::<iphdr>()) > ctx.data_end() {
+        info!(&ctx, "Iphdr is out of bounds");
+        return Ok(TC_ACT_OK);
     }
+    
+    let mut full_cksum = unsafe { bpf_csum_diff(0 as *mut _,0,ptr_at(&ctx, ETH_HDR_LEN)?, size_of::<iphdr>() as u32, 0)} as u64;
+    info!(&ctx, "full check = {:X}", full_cksum);
 
-    info!(&ctx, "Received a packet destined for svc ip: {} at port: {}", u32::from_be(ip_hdr.daddr), u16::from_be(udp_hdr.dest));
+    let folded_cksum = csum_fold_helper(full_cksum);
+    info!(&ctx, "folded check = {:X}", folded_cksum);
 
-    // // Load Src/Dest IPs, update IP cksum, Zero Out UDP Cksum
-    // ip_hdr.daddr = backend.daddr.to_be();
-    // ip_hdr.check = 0;
-    // ctx.l3_csum_replace(ETH_HDR_LEN + offset!(iphdr, check), 0, &ip_hdr, size_of(struct iphdr));
+    unsafe { (*ip_hdr).check = folded_cksum };
+    //unsafe { (*ip_hdr).check = bpf_csum_diff(0 as *mut _,0,ptr_at(&ctx, ETH_HDR_LEN)?, size_of::<iphdr>() as u32, 0) as u16; }
+    //ctx.l3_csum_replace(ip_hdr + offset_of(iphdr_daddr), 0, raw_iphdr as u64, size_of::<iphdr>() as u64);
+    // ip_hdr.check = bpf_csum_diff(0,0,raw_iphdr, size_of::<iphdr>() as u32,0);
+    info!(&ctx, "Updated Iphdr check = {:X}", unsafe { (*ip_hdr).check });
     // udp_hdr.dest = (backend.dport as u16).to_be();
-    // udp_hdr.check = 0;
-    // //Update IP Cksum
+    unsafe { (*udp_hdr).dest = (backend.dport as u16).to_be() };
+    unsafe { (*udp_hdr).check = 0};
+    // // Reload iphdr and udphdr with updates
+    // ctx.store(ETH_HDR_LEN, &ip_hdr, 0);
+    // ctx.store(udp_header_offset, &udp_hdr, 0);
 
-    Ok(0)
+    Ok(TC_ACT_OK)
 }
 
 #[panic_handler]
